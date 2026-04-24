b5fbc38fe41d5f00cdb6eb3f9b14394c9bd8b416
ARDIILLA26 <redesmexico4@gmail.com>
2026-04-23 19:43:14 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 const VERIFY_TOKEN = "caceres123";
 
-const ACCESS_TOKEN = "AQUI_TU_TOKEN_LARGO";
+const ACCESS_TOKEN = "EAAVJmkSwXmUBRejKPCZAADj1h40pnsnmQi5vyuoIx4Y4lZBtMGjvKZCBjIO0ZA5cPjB3TkWt1zXO7sVFSZCJuhTWc1ZB9u2k0IANa08xg81d6yYpyGoZCsmI1UxqDCMKalaHBUKnb4FARmbJFoiBIrHCZCRnpgZCgVlIgQNQX4l0t87TqhFudvP5jaJL5WWylXnWUZAd3F7alGCjBazcptplZBZA7oFzkC1jXvPqi4KJjPveK9luF09uA6p75vYjaijDZAg1yZBaTGAqFDiDIKmUmXQLIlxZACA;
 const PHONE_NUMBER_ID = "1129641810224240";
 
 app.get("/webhook", (req, res) => {
