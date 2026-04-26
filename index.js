899c2f86683f7b2a4b7abda08e500270c8cd9869
Franklin <tu_correo@gmail.com>
2026-04-25 18:48:16 -0600

    fix src index paths

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const http = require("http");
-const { createServerHandler } = require("./src/services/httpServer");
-const { ensureDataFiles } = require("./src/services/storageService");
-const { loadEnvFile } = require("./src/utils/env");
+
+const { createServerHandler } = require("./services/httpServer");
+const { ensureDataFiles } = require("./services/storageService");
+const { loadEnvFile } = require("./utils/env");
 
 loadEnvFile();
 ensureDataFiles();
 
-const port = Number(process.env.PORT || 3000);
+const port = Number(process.env.PORT) || 10000;
 const server = http.createServer(createServerHandler());
 
-server.listen(port, () => {
+server.listen(port, "0.0.0.0", () => {
   console.log(`WhatsApp chatbot escuchando en puerto ${port}`);
 });
